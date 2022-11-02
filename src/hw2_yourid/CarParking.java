30fe771b8f896e45230a1bb5fd7c25b198bd3106
Chandramal <105287790+Chandramal@users.noreply.github.com>
2022-11-02 17:49:41 +0530

    make remove function

--- src/hw2_yourid/CarParking.java
+++ src/hw2_yourid/CarParking.java
@@ -15,7 +15,10 @@ import java.util.Date;
  */
 public class CarParking {
 
-    public static Vehicle[][][] slots = new Vehicle[3][7][7];
+    static int floor = 3;
+    static int row = 7;
+    static int col = 7;
+    public static Vehicle[][][] slots = new Vehicle[floor][row][col];
 
     public Vehicle[][][] getSlots() {
         return slots;
@@ -24,15 +27,15 @@ public class CarParking {
     public void setSlots(Vehicle[][][] slots) {
         this.slots = slots;
     }
-    
-    public static void isOccupied(int floor, int row, int col){
-        if(slots[floor][row][col] == null){
-            System.out.println("index is null");
-            
+
+    public static boolean isOccupied(int floor, int row, int col) {
+        if (slots[floor][row][col] == null) {
+            return false;
+        } else {
+            return true;
         }
     }
-    
-    
+
     public static void main(String[] args) {
         //CarParking cp = new CarParking();
         Vehicle v = new Vehicle();
@@ -42,8 +45,32 @@ public class CarParking {
         isOccupied(2, 2, 2);
         //cp.isOccupied(3, 2, 2);
     }
+
+    public static void add(Vehicle v) {
+        int floor = 0;
+        int row = 0;
+        int col = 0;
+
+        for (floor = 0; floor < CarParking.floor; floor++) {
+            for (row = 0; row < CarParking.row; row++) {
+                for (col = 0; col < CarParking.col; col++) {
+                    if (isOccupied(floor, row, col)) {
+                        slots[floor][row][col] = v;
+                    }
+                }
+            }
+        }
+    }
     
-    public static void add(){
+    public double remove(int floor, int row, int col, double rate){
         
+        Vehicle v = slots[floor][row][col];
+        double dur = v.getDuration(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()))/60000;
+        double fee = dur * rate;
+        if(floor != 0){
+            fee = fee - (fee * 5 /100);
+        }
+        slots[floor][row][col] = null;
+        return fee;
     }
 }
